e603049c0ae41bbf31c8ccfb3fb3a6ac6002d4dd
karthik-siru <karthiksiruvalam@gmail.com>
2021-09-14 14:51:02 +0530

    states to update values dynamically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const Clicked = () => {
+    setTitle("Updated!");
+    console.log("title");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{"$" + props.price}</div>
       </div>
+      <button onClick={Clicked}>Click Me</button>
     </div>
   );
 };
