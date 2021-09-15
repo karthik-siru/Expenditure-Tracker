c4881aab6268fad252f2dc77f6708961cd785007
karthik-siru <karthiksiruvalam@gmail.com>
2021-09-15 23:07:55 +0530

    chart and chart bar added and finished

--- src/App.js
+++ src/App.js
@@ -3,32 +3,7 @@ import Expenses from "./components/Expenses/Expenses";
 import Card from "./components/UI/Card";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const INITIAL_EXPENSES = [
-  {
-    id: "e1",
-    title: "Car Insurance 1",
-    price: "400",
-    date: new Date(2021, 3, 11),
-  },
-  {
-    id: "e2",
-    title: "Car Insurance 2",
-    price: "500",
-    date: new Date(2021, 3, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance 3",
-    price: "600",
-    date: new Date(2021, 3, 13),
-  },
-  {
-    id: "e4",
-    title: "Car Insurance 4",
-    price: "700",
-    date: new Date(2021, 3, 15),
-  },
-];
+const INITIAL_EXPENSES = [];
 
 const App = () => {
   const [expenses, AppendExpense] = useState(INITIAL_EXPENSES);
